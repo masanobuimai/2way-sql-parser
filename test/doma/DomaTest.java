b65792d01d6bb28273f9cf58bb97f33ba1f45c09
masanobu.imai <masanobu.imai@gmail.com>
2012-04-17 18:46:34 +0900

    Domaの扱いがあんまりだったので，コメント書いておいた。

--- test/doma/DomaTest.java
+++ test/doma/DomaTest.java
@@ -1,14 +1,17 @@
 package doma;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.seasar.doma.internal.jdbc.sql.SqlParser;
 import org.seasar.doma.jdbc.SqlNode;
 
 public class DomaTest {
     @Test
+    @Ignore
     public void testSample() {
         SqlParser parser = new SqlParser("select * from employee");
         SqlNode sqlNode = parser.parse();
+        // Domaは既存のVisitorを流用できなそうなので，自力でVisitor作らないとダメっぽい。
         System.out.println("sqlNode = " + sqlNode);
     }
 }
